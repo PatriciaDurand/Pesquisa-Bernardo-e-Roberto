42a5b336b8ef9bab9e834d641299633ed7d23e21
PatriciaDurand <durand_patricia_@hotmail.com>
2015-01-29 14:21:58 -0200

    Adicionando as mensagens iniciais do sistema

--- src/App.java
+++ src/App.java
@@ -1,8 +1,27 @@
+import java.util.Scanner;
+
 
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("Pense em um desses animais:");
+		System.out.println("Andorinha");
+		System.out.println("Papagaio");
+		System.out.println("Pavão");
+		System.out.println("Baleia");
+		System.out.println("Peixe");
+		System.out.println("Lagarto");
+		System.out.println("Camaleão");
+		System.out.println("Urso");
+		System.out.println("Macaco");
+		
+		System.out.println("Digite o animal em que você pensou");
+		Scanner sc = new Scanner(System.in);
+		String animal = sc.next();
+		
+		System.out.println("Agora eu vou tentar adivinhar");
+				
 
 	}
 
